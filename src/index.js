900159f08c9498efe7ea56456f91aef35825ba3f
Travis Crabbe <travcrabbe@gmail.com>
2023-11-27 22:34:09 -0800

    Finished setting up a basic bot which can respond to messages.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
-// const { Client, IntentsBitField } = require(discord.js);
-const fs = require('fs');
+const { Client, IntentsBitField } = require("discord.js");
+const fs = require("fs");
+let token;
 try {
-    const token = readConfig();
-    console.log('Token:', token);
+    token = readConfig();
 } catch (err) {
-    console.error('Error:', err);
+    console.error("Error:", err);
 }
-// const client = new Client({
-//     intents:    [
-//         IntentsBitField.Flags.Guild,
-//         IntentsBitField.Flags.GuildMembers,
-//         IntentsBitField.Flags.GuildMessages,
-//         IntentsBitField.Flags.MessageContent
-//     ],
-// });
+const client = new Client({
+    intents:    [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ],
+});
+
+client.on("ready", (c) => {
+    console.log(`${c.user.tag} is online`);
+})
+
+client.on('messageCreate', (message) => {
+
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === "ping!") {
+        message.reply("pong!");
+    }
+})
+
+client.login(token);
 
 function readConfig() {
     try {
-        const data = fs.readFileSync('D:\\repositories\\birthdayBot\\config.json', 'utf-8');
+        const data = fs.readFileSync('config.json', 'utf-8');
         const jsonData = JSON.parse(data);
         return jsonData.token;
     } catch (err) {
-        console.error('Error reading file:', err);
+        console.error("Error reading file:", err);
         throw err;
     }
 }
